31d7efeff0e100bc4310e4d214be3c7253b15be3
thomasf04 <113307948+thomasf04@users.noreply.github.com>
2022-09-26 09:13:25 +0100

    Update SquareMatrix.cs

--- SquareMatrix.cs
+++ SquareMatrix.cs
@@ -11,23 +11,49 @@ namespace Technical_Solution
             this.size = size;
             rows = size;
             columns = size;
-            elements = new double[size, size];
+            elements = new int[size, size];
             for (int i = 0; i < size; i++)
             {
                 elements[i, i] = 1;
             }
         }
 
-        public SquareMatrix(double[,] elements) : base(elements)
+        public SquareMatrix(int[,] elements) : base(elements)
         {
             this.elements = elements;
             size = elements.GetLength(0);
         }
 
-        public Matrix Inverse()
+        public SquareMatrix Inverse()
         {
             Matrix augMatrix = new Matrix(size, 2 * size);
-            double x;
+            SquareMatrix inverseMatrix = new SquareMatrix(size);
+            int[] ModularInverses = new int[] { 0, 1, 0, 9, 0, 21, 0, 15, 0, 3, 0, 19, 0, 0, 0, 7, 0, 23, 0, 11, 0, 5, 0, 17, 0, 25 };
+            int x;
+
+            //C# '%' operator returns negative values
+            int mod(int a)
+            {
+                if (a < 0)
+                {
+                    return a - 26 * (int)(a / 26 - 1);
+                }
+                return a % 26;
+            }
+
+            void SwapRowsUntilLegalX(int k)
+            {
+                int x;
+                int newK = k;
+                do
+                {
+                    x = augMatrix.elements[newK, k];
+                    newK += 1;
+                } while (x % 2 == 0 || x % 13 == 0);
+
+                augMatrix.SwapRows(k, newK-1);
+            }
+            
 
             //Fill augMatrix with original matrix
             for (int i = 0; i < size; i++)
@@ -43,20 +69,25 @@ namespace Technical_Solution
             {
                 augMatrix.elements[i, i + size] = 1;
             }
-            Console.WriteLine(true);
-            augMatrix.PrintMatrix();
 
-
+            //Into echelon form
             for (int k = 0; k < size; k++)
             {
-                x = augMatrix.elements[k, k];
+                x = mod(augMatrix.elements[k, k]);
+                if (x % 2 == 0 || x % 13 == 0)
+                {
+                    SwapRowsUntilLegalX(k);
+                }
+                x = mod(augMatrix.elements[k, k]);
+
+
                 if (x == 0)
                 {
                     throw new ZeroDeterminantException();
                 }
                 for (int j = 0; j < 2 * size; j++)
                 {
-                    augMatrix.elements[k, j] /= x;
+                    augMatrix.elements[k, j] *=  ModularInverses[x];
                 }
 
                 for (int i = k+1; i < size; i++)
@@ -68,7 +99,7 @@ namespace Technical_Solution
                     }
                 }
             }
-            augMatrix.PrintMatrix();
+            //Back substitution
             for (int k = 0; k < size; k++)
             {
                 for (int j = k+1; j < size; j++)
@@ -81,8 +112,17 @@ namespace Technical_Solution
                 }
             }
 
+            //Fill inverse matrix with right-hand side of augMatrix
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    inverseMatrix.elements[i, j] = mod(augMatrix.elements[i, j + size]);
+                }
+            }
 
-            return augMatrix;
+
+            return inverseMatrix;
         }
 
 
